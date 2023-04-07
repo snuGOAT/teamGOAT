2433bb6ea13d9c7283965f447731f13d42740d1e
snuGOAT <yunsu0915@gmail.com>
2023-04-07 11:58:31 +0900

    ak: sadded hi to main

---
+++ calc.py
@@ -0,0 +1,20 @@
+def add(a,b):
+	return a+b
+def mul(a,b):
+	return a*b
+def sub(a,b):
+	return a-b
+def div(a,b):
+	if(b==0):
+		print('Error')
+		quit()
+	else:
+	return a/b
+	
+if __name__=='__main__':
+	x=int(input('x>'))
+	y=int(input('y>'))
+	print('%d + %d = %d'%(x,y, add(x,y)))
+	print('%d - %d = %d'%(x,y, sub(x,y)))
+	print('%d * %d = %d'%(x,y, mul(x,y)))
+	print('%d / %d = %d'%(x,y, div(x,y)))
