8c1fa3c5ec40fe233b0514c181bbf20665df198d
snuGOAT <130116321+snuGOAT@users.noreply.github.com>
2023-05-16 18:37:20 +0900

    Add files via upload

---
+++ mb.py
@@ -0,0 +1,55 @@
+import rospy
+import numpy as np
+import cv2
+from sensor_msgs.msg import Image, CompressedImage
+from cv_bridge import CvBridge, CvBridgeError
+from std_msgs.msg import Header
+
+class DetermineColor:
+    def __init__(self):
+        self.image_sub = rospy.Subscriber('/camera/color/image_raw', Image, self.callback)
+        self.color_pub = rospy.Publisher('/rotate_cmd', Header, queue_size=10)
+        self.bridge = CvBridge()
+        self.count = 0
+
+    def callback(self, data):
+        try:
+            # listen image topic
+            image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+            #cv2.imshow('Image',image)
+            
+
+            # prepare rotate_cmd msg
+            # DO NOT DELETE THE BELOW THREE LINES!
+            msg = Header()
+            msg = data.header
+            msg.frame_id = '0'  # default: STOP
+		
+            self.count+=1
+
+            # determine background color
+            # TODO
+            # determine the color and assing +1, 0, or, -1 for frame_id
+            # msg.frame_id = '+1' # CCW (Blue background)
+            # msg.frame_id = '0'  # STOP
+            # msg.frame_id = '-1' # CW (Red background)
+
+		
+            # publish color_state
+            self.color_pub.publish(msg)
+
+        except CvBridgeError as e:
+            print(e)
+
+
+    def rospy_shutdown(self, signal, frame):
+        rospy.signal_shutdown("shut down")
+        sys.exit(0)
+
+if __name__ == '__main__':
+    rospy.init_node('CompressedImages1', anonymous=False)
+    detector = DetermineColor()
+    
+    rospy.spin()
+
+
